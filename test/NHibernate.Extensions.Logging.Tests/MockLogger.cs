de32e75cbe6684cb6858a7e7158e1a4af2de7b60
Charley Wu <akunzai@gmail.com>
2019-02-27 13:08:24 +0800

    Fix coding style

--- test/NHibernate.Extensions.Logging.Tests/MockLogger.cs
+++ test/NHibernate.Extensions.Logging.Tests/MockLogger.cs
@@ -1,12 +1,25 @@
-﻿using System;
+using System;
 using Microsoft.Extensions.Logging;
 
 namespace NHibernate.Extensions.Logging.Tests
 {
     public class MockLogger : ILogger
     {
+        public MockLogger()
+        {
+        }
+
+        public MockLogger(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
         public object State { get; set; }
+
         public LogLevel LogLevel { get; set; }
+
         public Exception Exception { get; set; }
 
         public IDisposable BeginScope<TState>(TState state)
