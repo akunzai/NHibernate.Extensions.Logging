4e0b7b43c22e19e53943ef081e3f03b956db6e75
Charley Wu <akunzai@gmail.com>
2023-07-13 20:43:53 +0800

    Use file scoped namespaces
    - https://learn.microsoft.com/dotnet/csharp/language-reference/proposals/csharp-10.0/file-scoped-namespaces

--- samples/SampleShared/Mapping/TodoMap.cs
+++ samples/SampleShared/Mapping/TodoMap.cs
@@ -2,17 +2,16 @@
 using NHibernate.Type;
 using SampleShared.Domain;
 
-namespace SampleShared.Mapping
+namespace SampleShared.Mapping;
+
+public class TodoMap : ClassMap<Todo>
 {
-    public class TodoMap : ClassMap<Todo>
+    public TodoMap()
     {
-        public TodoMap()
-        {
-            Table("Todos");
-            Id(x => x.Id).GeneratedBy.GuidComb();
-            Map(x => x.Title).Not.Nullable();
-            Map(x => x.Completed).Not.Nullable();
-            Map(x => x.Created).CustomType<LocalDateTimeType>();
-        }
+        Table("Todos");
+        Id(x => x.Id).GeneratedBy.GuidComb();
+        Map(x => x.Title).Not.Nullable();
+        Map(x => x.Completed).Not.Nullable();
+        Map(x => x.Created).CustomType<LocalDateTimeType>();
     }
 }
--- test/NHibernate.Extensions.Logging.Tests/MockLogger.cs
+++ test/NHibernate.Extensions.Logging.Tests/MockLogger.cs
@@ -1,42 +1,41 @@
 using System;
 using Microsoft.Extensions.Logging;
 
-namespace NHibernate.Extensions.Logging.Tests
+namespace NHibernate.Extensions.Logging.Tests;
+
+public class MockLogger : ILogger
 {
-    public class MockLogger : ILogger
+    public MockLogger()
     {
-        public MockLogger()
-        {
-        }
+    }
 
-        public MockLogger(string name)
-        {
-            Name = name;
-        }
+    public MockLogger(string name)
+    {
+        Name = name;
+    }
 
-        public string Name { get; set; }
+    public string Name { get; set; }
 
-        public object State { get; set; }
+    public object State { get; set; }
 
-        public LogLevel LogLevel { get; set; }
+    public LogLevel LogLevel { get; set; }
 
-        public Exception Exception { get; set; }
+    public Exception Exception { get; set; }
 
-        public IDisposable BeginScope<TState>(TState state)
-        {
-            throw new NotImplementedException();
-        }
+    public IDisposable BeginScope<TState>(TState state)
+    {
+        throw new NotImplementedException();
+    }
 
-        public bool IsEnabled(LogLevel logLevel)
-        {
-            return true;
-        }
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return true;
+    }
 
-        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
-        {
-            LogLevel = logLevel;
-            State = state;
-            Exception = exception;
-        }
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+    {
+        LogLevel = logLevel;
+        State = state;
+        Exception = exception;
     }
 }
